f79d2fcf2a3a0fba67fb01aa09b02d5df1d97a5d
Jacob Poulsgaard Tjørnholm <chopmo@gmail.com>
2015-10-25 14:36:44 +0100

    Too compact?

--- src/living/core.clj
+++ src/living/core.clj
@@ -1,12 +1,9 @@
 (ns living.core)
 
-(defn update-smallest [map]
-  (let [min-pair (first (sort-by last map))
-        [min-key min-val] min-pair]
-    (assoc map min-key (+ min-val min-key))))
-
 (defn lcm [& xs]
-  (loop [map (zipmap xs xs)]
-    (if (apply = (vals map))
-      (first (vals map))
-      (recur (update-smallest map)))))
+  (let [result-or-nil (fn [map] (when (apply = (vals map))(first (vals map))))
+        update-map (fn [map] (let [min-pair (first (sort-by last map))
+                                [min-key min-val] min-pair]
+                            (assoc map min-key (+ min-val min-key))))]
+   (loop [map (zipmap xs xs)]
+     (or (lcm-result map) (recur (update-smallest map))))))
