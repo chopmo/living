0412da2c54ce11f2161298f150c9943c0746c869
Jacob Poulsgaard Tjørnholm <chopmo@gmail.com>
2015-11-13 21:28:25 +0100

    Balanced trees

--- src/living/core.clj
+++ src/living/core.clj
@@ -1,35 +1,45 @@
 (ns living.core)
 
-(defn lcm [& xs]
-  (let [result-or-nil (fn [map] (when (apply = (vals map))(first (vals map))))
-        update-map (fn [map] (let [min-pair (first (sort-by last map))
-                                [min-key min-val] min-pair]
-                            (assoc map min-key (+ min-val min-key))))]
-   (loop [map (zipmap xs xs)]
-     (or (lcm-result map) (recur (update-smallest map))))))
+;; (defn lcm [& xs]
+;;   (let [result-or-nil (fn [map] (when (apply = (vals map))(first (vals map))))
+;;         update-map (fn [map] (let [min-pair (first (sort-by last map))
+;;                                 [min-key min-val] min-pair]
+;;                             (assoc map min-key (+ min-val min-key))))]
+;;    (loop [map (zipmap xs xs)]
+;;      (or (lcm-result map) (recur (update-smallest map))))))
 
-;; Recursive
-(defn pascal [n]
-  (if (= 1 n)
-    [1]
-    (let [prev-list (pascal (dec n))
-          partitions (partition 2 1 prev-list)
-          updated-list (map (partial apply +) partitions)]
-      (concat [1] updated-list [1]))))
+;; ;; Recursive
+;; (defn pascal [n]
+;;   (if (= 1 n)
+;;     [1]
+;;     (let [prev-list (pascal (dec n))
+;;           partitions (partition 2 1 prev-list)
+;;           updated-list (map (partial apply +) partitions)]
+;;       (concat [1] updated-list [1]))))
 
-;; Iteration
-(defn pascal [n]
-  (loop [iteration 1
-         list [1]]
-    (if (= n iteration)
-      list
-      (let [partitions (partition 2 1 list)
-            reduced-list (map (partial apply +) partitions)
-            new-list (concat [1] reduced-list [1])]
-        (recur (inc iteration) new-list)))))
+;; ;; Iteration
+;; (defn pascal [n]
+;;   (loop [iteration 1
+;;          list [1]]
+;;     (if (= n iteration)
+;;       list
+;;       (let [partitions (partition 2 1 list)
+;;             reduced-list (map (partial apply +) partitions)
+;;             new-list (concat [1] reduced-list [1])]
+;;         (recur (inc iteration) new-list)))))
 
-(fn tree? [node]
-  (boolean (or (nil? node)
-       (if (and (coll? node) (= 3 (count node)))
-         (let [[value left right] node]
-           (and value (tree? left) (tree? right)))))))
+;; (defn tree? [node]
+;;   (boolean (or (nil? node)
+;;        (if (and (coll? node) (= 3 (count node)))
+;;          (let [[value left right] node]
+;;            (and value (tree? left) (tree? right)))))))
+
+
+(def balanced-tree? (fn [node]
+   (letfn [(mirror [node]
+             (if (nil? node) nil
+                 (let [[value left right] node]
+                   [value (mirror right) (mirror left)])))]
+
+     (let [[value left right] node]
+       (= left (mirror right))))))
--- test/living/core_test.clj
+++ test/living/core_test.clj
@@ -2,10 +2,37 @@
   (:require [clojure.test :refer :all]
             [living.core :refer :all]))
 
-(deftest test-lcj
-  (testing "LCM function"
-    (is (= (lcm 2 3) 6))
-    (is (= (lcm 5 3 7) 105))
-    (is (= (lcm 1/3 2/5) 2))
-    (is (= (lcm 3/4 1/6) 3/2))
-    (is (= (lcm 7 5/7 2 3/5) 210))))
+;; (deftest test-lcj
+;;   (testing "LCM function"
+;;     (is (= (lcm 2 3) 6))
+;;     (is (= (lcm 5 3 7) 105))
+;;     (is (= (lcm 1/3 2/5) 2))
+;;     (is (= (lcm 3/4 1/6) 3/2))
+;;     (is (= (lcm 7 5/7 2 3/5) 210))))
+
+;; (deftest test-tree?
+;;   (testing "Tree predicate"
+;;     (is (= (tree? '(:a (:b nil nil) nil)) ))))
+
+(deftest test-balanced-tree?
+  (testing "Balanced tree predicate"
+    (let [__ balanced-tree?]
+      (is (= (__ '(:a (:b nil nil) (:b nil nil))) true))
+      (is (= (__ '(:a (:b nil nil) nil)) false))
+      (is (= (__ '(:a (:b nil nil) (:c nil nil))) false))
+      (is (= (__ [1
+                  [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
+                  [2 [3 nil [4 [6 nil nil] [5 nil nil]]] nil]]) true))
+      (is (= (__ [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
+                  [2 [3 nil [4 [5 nil nil] [6 nil nil]]] nil]]) false))
+
+      (is (= (__ [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] ]]
+                  [2 [3 nil [4 [6 nil nil] nil]] nil]]) false)))))
+
+;; (deftest test-flipping
+;;   (let [__ flip]
+;;     (testing "Higher order function to flip args"
+;;       (is (= 3 ((__ nth) 2 [1 2 3 4 5])))
+;;       (is (= true ((__ >) 7 8)))
+;;       (is (= 4 ((__ quot) 2 8)))
+;;       (is (= [1 2 3] ((__ take) [1 2 3 4 5] 3))))))
